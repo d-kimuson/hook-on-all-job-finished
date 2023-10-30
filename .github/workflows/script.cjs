d18018273ee220b77d86b80df32a2716d48f1e9d
d-kimsuon <dev.kimuson@gmail.com>
2023-10-30 10:08:12 +0900

    wip

--- .github/workflows/script.cjs
+++ .github/workflows/script.cjs
@@ -4,15 +4,18 @@
  * @typedef {import("@octokit/rest").Octokit} OctokitClient
  * @link https://octokit.github.io/rest.js/v20#usage
  */
+
 /** @typedef {import("@actions/github").context} WorkflowRunContext */
 
 /** @type {(arg: { github: OctokitClient, context: WorkflowRunContext }) => Promise<void>} */
 module.exports = async ({ github, context }) => {
-  const list = await github.checks.listForRef({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: context.ref,
-  });
+  // const list = await github.checks.listForRef({
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   ref: context.ref,
+  // });
   console.log("context", context);
-  console.log("list", list);
+  // console.log("list", list);
+
+  console.log("github", github);
 };
